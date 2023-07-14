aa4862f6d5256034f4b020e112a2508a494824c8
harshit <harshit.srivastava>
2023-07-14 16:06:21 +0530

    Added animations to svg image and applying it on background

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Header from '@/components/Header'
+import BackgroundImage from '../components/BackgroundImage';
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <Header/>
-      <div className='flex items-center justify-center min-h-screen mt-10 bg-slate-300 rounded-2xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl mx-auto w-11/12'>
-        Probably the hero section...
-      </div>
-    </main>
+      <main className="flex flex-col">
+        <BackgroundImage />
+        <Header/>
+        <div className='flex items-center justify-center min-h-screen mt-10 bg-slate-300 rounded-2xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl mx-auto w-11/12'>
+          Probably the hero section...
+        </div>
+      </main>
   )
 }
